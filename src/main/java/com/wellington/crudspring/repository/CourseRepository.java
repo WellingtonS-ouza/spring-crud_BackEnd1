05909ac09c2236ae6814a9e64a2ddbd2b0212f9b
Wellington <wel.souza88@gmail.com>
2024-01-30 23:33:59 -0300

    first commit

---
+++ src/main/java/com/wellington/crudspring/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.wellington.crudspring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wellington.crudspring.model.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
